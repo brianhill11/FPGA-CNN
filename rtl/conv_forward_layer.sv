e2b4b5e37c1ba3f0d574847855153e4ca37d772c
Brian Hill <hillb@oregonstate.edu>
2016-02-08 22:53:14 -0800

    Working version of conv_forward, including bias_term addition

--- rtl/conv_forward_layer.sv
+++ rtl/conv_forward_layer.sv
@@ -1,94 +1,52 @@
-module conv_forward_layer #(parameter WIDTH = 8)
+module conv_forward_layer #(parameter WIDTH = 16)
 									(
 										input logic					clk,
 										input logic					reset,
 										input logic					enable,
 										input logic		[31:0] 	in_data 	[WIDTH-1:0],
 										input logic		[31:0]	weights	[WIDTH-1:0],
+										input logic		[31:0]	bias_term,
 										output logic	[31:0] 	out_data
 									);
 										
 	logic [31:0] connections [2*WIDTH] ;
-	reg [31:0] final_result;
 	
 	genvar i, j;
-	int k;
 	generate 
 		//create float_mult blocks to multiply WIDTH number 
 		//of inputs with weights
 		for (i = 0; i < WIDTH; i++) begin : GEN_MULTS
-			wire [31:0] result;
+			wire [31:0] results;
 			float_mult float_mult_inst(
 												.clk_en(enable),
 												.clock(clk),
 												.dataa(in_data[i]),
 												.datab(weights[i]),
-												.result(connections[i])
+												.result(connections[i+WIDTH])
 												);
 		end 
-		//need input from 2 previous blocks so number of 
-		//blocks in each next layer is half of previous
-		//layer 1
-		for (i = 0; i < WIDTH/2; i++) begin : GEN_SUMS1
+		//sum the products, and reduce to single value
+		for (i = WIDTH; i > 1; i = i / 2) begin : GEN_SUMS
+			for (j = i; j > i/2 && j != 1; j--) begin : SUM_MULTS
 					float_add float_add_inst(
 												 .aclr(!enable),
 												 .clock(clk),
-												 .dataa(connections[i]),
-												 .datab(connections[i+WIDTH/2]),
-												 .result(connections[i+WIDTH])
+												 .dataa(connections[2*j-1]),
+												 .datab(connections[2*j-2]),
+												 .result(connections[j-1])
 												 );
+			end
 		end
-		//layer 2
-		for (i = WIDTH; i < WIDTH+WIDTH/4; i++) begin : GEN_SUMS2
-					float_add float_add_inst(
-												 .aclr(!enable),
-												 .clock(clk),
-												 .dataa(connections[i]),
-												 .datab(connections[i+WIDTH/4]),
-												 .result(connections[i+WIDTH/2])
-												 );
-		end
-		//layer 3
-		for (i = WIDTH+WIDTH/2; i < WIDTH+WIDTH/2+WIDTH/8; i++) begin : GEN_SUMS3
-					float_add float_add_inst(
-												 .aclr(!enable),
-												 .clock(clk),
-												 .dataa(connections[i]),
-												 .datab(connections[i+WIDTH/8]),
-												 .result(connections[i+WIDTH/4])
-												 );
-		end
-//		for (i = 1; i < WIDTH; i = i * 2) begin : GEN_SUMS
-//			wire [31:0] results [i];
-//			//for each block in new layer, connect to 2 prev 
-//			//if first iteration, connect with result from mult
-//			if (i == 1) begin
-//				for (j = 0; j < i; j++) begin : SUM_MULTS
-//					float_add float_add_inst(
-//												 .aclr(!enable),
-//												 .clock(clk),
-//												 .dataa(GEN_SUMS[1].REDUCE_SUMS[0].float_add_inst.result),
-//												 .datab(GEN_SUMS[1].REDUCE_SUMS[1].float_add_inst.result),
-//												 .result(final_result)
-//												 );
-//				end
-//			//else connect to result from addition
-//			end else begin
-//				for (j = 0; j < i; j++) begin : REDUCE_SUMS
-//					float_add float_add_inst(
-//												 .aclr(!enable),
-//												 .clock(clk),
-//												 .dataa(GEN_SUMS[i]..result),
-//												 .datab(GEN_MULTS[j+i].result),
-//												 //.result(results[j])
-//												 );
-//				end
-//			
-//			end
-//		end
-		
-
 	endgenerate
+	
+	//add bias term to sum to produce final sum
+	float_add float_add_bias_term(
+												 .aclr(!enable),
+												 .clock(clk),
+												 .dataa(connections[1]),
+												 .datab(connections[bias_term]),
+												 .result(connections[0])
+												 );
 	
 		always @(posedge clk) begin
 			out_data <= connections[0];
