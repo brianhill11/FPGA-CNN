82ea82394f4c116f4e98047f2570e4fef7611635
Brian Hill <hillb@pelican03.eecs.oregonstate.edu>
2016-04-12 19:23:40 -0700

    Added script that uses nvidia-smi command to gather metrics from GPUs

---
+++ tools/nvidia_smi_command.sh
@@ -0,0 +1,5 @@
+#!/bin/sh 
+
+# this command is used to query the Nvidia GPUs for various metrics 
+
+nvidia-smi --query-gpu=timestamp,index,pstate,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,power.draw,power.limit,clocks.gr,clocks.sm,clocks.mem,clocks.applications.gr,clocks.applications.mem,gpu_uuid -l 3 -f gpu_metric_dump.csv --format=csv,nounits
