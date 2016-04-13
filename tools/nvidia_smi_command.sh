2da9141b21b06d2b21581a96087d6b81ae397587
Brian Hill <hillb@pelican03.eecs.oregonstate.edu>
2016-04-12 19:26:40 -0700

    Changed nvidia-smi query time from 3 seconds to 10

--- tools/nvidia_smi_command.sh
+++ tools/nvidia_smi_command.sh
@@ -2,4 +2,4 @@
 
 # this command is used to query the Nvidia GPUs for various metrics 
 
-nvidia-smi --query-gpu=timestamp,index,pstate,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,power.draw,power.limit,clocks.gr,clocks.sm,clocks.mem,clocks.applications.gr,clocks.applications.mem,gpu_uuid -l 3 -f gpu_metric_dump.csv --format=csv,nounits
+nvidia-smi --query-gpu=timestamp,index,pstate,memory.total,memory.used,memory.free,utilization.gpu,utilization.memory,power.draw,power.limit,clocks.gr,clocks.sm,clocks.mem,clocks.applications.gr,clocks.applications.mem,gpu_uuid -l 10 -f gpu_metric_dump.csv --format=csv,nounits
